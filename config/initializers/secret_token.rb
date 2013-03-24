026a916b45bef2c709e26a6387759b731010e997
ruting <liuruting@gmail.com>
2013-03-24 13:47:57 +0800

    Full Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridetogether::Application.config.secret_token = 'cea084d4c577b9d5b4b681641ce4261950fb7febd031ce47c8932b45cfc1f3d5ec76ff5983682152d3b8fc5968701a8b42efd88a3f95677658161e2ca1d6d74f'
